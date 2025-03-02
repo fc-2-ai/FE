5fdb64ca5293509fb0870965225c55ff2e969a43
santa-developer <finalatro37@naver.com>
2025-03-02 23:08:28 +0900

    Initial commit

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset} 
+  
+// 추가 전역 스타일 작성
+`;
+
+export default GlobalStyles;
