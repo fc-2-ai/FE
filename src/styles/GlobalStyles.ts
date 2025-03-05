88f3c8c2ec93356e7e8747ed211f6f08bebfe667
santa-developer <finalatro37@naver.com>
2025-03-05 17:15:04 +0900

    design: 폰트 설정 및 theme 생성

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,13 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import './font.css';
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+
+
 // 추가 전역 스타일 작성
 
     a {
     text-decoration: none;
     color: inherit;
+    }
+
+    body,html {
+    font-family: "NexonLv2", "Malgun Gothic", "맑은 고딕", helvetica, sans-serif;
+    font-size: ${({ theme }) => theme.fontSizes.fz16};
+    color: ${({ theme }) => theme.colors.text};
+    font-weight: ${({ theme }) => theme.fontWeights.regular}
     }
 
 `;
---
+++ src/styles/theme.ts
@@ -0,0 +1,109 @@
+const theme = {
+  // Colors
+  colors: {
+    white: '#fff',
+    black: '#000',
+    primary: '#0C2764',
+    secondary1: '#3372FF',
+    secondary2: '#365AF9',
+    error: '#FF402B',
+    background: '#F7F8FA',
+    background1: '#F6F6FA',
+    background2: '#EEF0F5',
+    lightGray1: '#E4E5E7',
+    lightGray2: '#CDCDCD',
+    darkGray1: '#9E9EAD',
+    darkGray2: '#8B8B9D',
+    darkGray3: '#6E6E84',
+    darkGray4: '#5D5D5D',
+    text: '#2E2E37',
+    textSecond: '#484554',
+    border1: '#E8E8E8',
+    border2: '#EAF0FD',
+    border3: '#BCBCBC',
+  },
+
+  // font-size
+  fontSizes: {
+    fz10: '10px',
+    fz12: '12px',
+    fz14: '14px',
+    fz16: '16px',
+    fz18: '18px',
+    fz20: '20px',
+    fz22: '22px',
+    fz24: '24px',
+    fz26: '26px',
+    fz28: '28px',
+    fz30: '30px',
+    fz32: '32px',
+    fz34: '34px',
+    fz36: '36px',
+    fz38: '38px',
+    fz40: '40px',
+    fz42: '42px',
+    fz44: '44px',
+    fz46: '46px',
+    fz48: '48px',
+    fz50: '50px',
+    fz52: '52px',
+    fz54: '54px',
+    fz56: '56px',
+    fz58: '58px',
+    fz60: '60px',
+  },
+
+  // font-weight
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+
+  // line-heights
+  lineHeights: {
+    small: '1.2',
+    medium: '1.5',
+    large: '1.8',
+  },
+
+  // radius
+  radius: {
+    small: '6px',
+    medium: '8px',
+    large: '10px',
+    xlarge: '12px',
+    xxlarge: '16px',
+    circle: '50%',
+  },
+
+  // border-width
+  borderWidth: {
+    thin: '1px',
+    light: '1.5px',
+    medium: '2px',
+    thick: '3px',
+    xThick: '4px',
+    xxThick: '5px',
+    heavy: '6px',
+    ultraHeavy: '8px',
+  },
+
+  // spacing
+  spacing: {
+    xs: '4px',
+    sm: '8px',
+    md: '16px',
+    lg: '24px',
+    xl: '32px',
+  },
+
+  // box-shadow
+  boxShadow: {
+    subtle: '0px 0px 5px rgba(46, 46, 55, 0.8)',
+    regular: '0px 4px 10px rgba(0, 0, 0, 0.8)',
+  },
+};
+
+export default theme;
