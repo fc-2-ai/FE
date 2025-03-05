a49afc080c2456eae7a2958f6b38ac70307491e6
santa-developer <finalatro37@naver.com>
2025-03-05 17:06:59 +0900

    conf: vite-plugin-svgr 사용 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    svgr({
+      // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+      include: '**/*.svg?react',
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
